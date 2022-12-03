89a3ae15f5bf69fa10254c8d6a925f486a14ba52
Evan McConnell <35697039+EvanMcConnell@users.noreply.github.com>
2022-12-03 22:59:26 +0000

    Add files via upload

---
+++ EvansWhackyExtensions.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public static class EvansWhackyExtensions
+{
+    /*
+     *     Vector3
+     */
+    public static Vector3 With(this Vector3 it, float? x = null, float? y = null, float? z = null)
+    {
+        return new Vector3(x ?? it.x, y ?? it.y, z ?? it.z);
+    }
+    
+    /*
+     *     Transform
+     */
+    public static void SetEulerAngle(this Transform it, float? x = null, float? y = null, float? z = null)
+    {
+        var newEulerAngles = it.eulerAngles;
+        it.eulerAngles = newEulerAngles.With(x: x ?? newEulerAngles.x, y: y ?? newEulerAngles.y, z: z ?? newEulerAngles.z);
+    }
+}
